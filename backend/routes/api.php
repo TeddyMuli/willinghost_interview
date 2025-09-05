05e45262a42e949d2490c57372ed9c9068015e7e
TeddyMuli <tmuli974@gmail.com>
2025-09-05 11:44:22 +0300

    add routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,8 @@
 <?php
+use App\Http\Controllers\BnBController;
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/bnbs', [BnBController::class, 'index']);
+Route::get('/bnbs/{id}', [BnBController::class, 'show']);
+Route::post('/bnbs', [BnBController::class, 'store']);
+Route::put('/bnbs/{id}', [BnBController::class, 'update']);
+Route::delete('/bnbs/{id}', [BnBController::class, 'destroy']);
